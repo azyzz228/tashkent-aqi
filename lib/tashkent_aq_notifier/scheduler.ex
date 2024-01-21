584bdd5b7e69cf3dd7dd34d09998d0580fefad8b
Aziz Abdullaev <as1036@duke.edu>
2024-01-21 23:34:32 +0500

    feat: integrate with notifier, refactor:change time function name

--- lib/tashkent_aq_notifier/scheduler.ex
+++ lib/tashkent_aq_notifier/scheduler.ex
@@ -1,5 +1,10 @@
 defmodule TashkentAqNotifier.Scheduler do
+  alias TashkentAqNotifier.Notifier
+  alias TashkentAqNotifier.Subscribers
+  alias TashkentAqNotifier.Subscribers.Subscriber
   use GenServer
+
+  @three_hours_in_ms 60 * 60 * 3 * 1000
 
   def start_link(_) do
     GenServer.start_link(__MODULE__, %{}, name: __MODULE__)
@@ -11,7 +16,7 @@ defmodule TashkentAqNotifier.Scheduler do
   end
 
   def handle_info(:broadcast, state) do
-    now = get_time_in_tashkent_now()
+    now = uz_time_now()
 
     if now.hour > 7 do
       broadcast()
@@ -26,34 +31,35 @@ defmodule TashkentAqNotifier.Scheduler do
   end
 
   def initiate_scheduler() do
-    now = get_time_in_tashkent_now()
-    minutes = now.minute - 1
-    later = now |> DateTime.add(1, :hour) |> DateTime.add(-minutes, :minute)
+    now = uz_time_now()
+    # minutes = now.minute - 7
+    # later = now |> DateTime.add(1, :hour) |> DateTime.add(-minutes, :minute)
+    later = now |> DateTime.add(1, :minute)
 
     ms_before_next_hour_01_minute = DateTime.diff(later, now, :millisecond)
     Process.send_after(self(), :broadcast, ms_before_next_hour_01_minute)
   end
 
-  defp broadcast() do
-    now = get_time_in_tashkent_now()
-    ids = [242_087_850, 6_169_337_963]
-
-    Enum.each(ids, fn id ->
+  def broadcast() do
+    Subscribers.list_active_subscribers()
+    |> Enum.each(fn %Subscriber{} = subscriber ->
       case Telegex.send_message(
-             id,
-             "Hello chat id #{id}, this is a broadcast, sent at #{now.hour}:#{now.minute}:#{now.second}"
+             subscriber.chat_id,
+             Notifier.get_notification_message()
            ) do
-        {:error, error} ->
-          # TODO: delete from DB if chat_id is not found (or just mark it as inactive)
-          dbg(error)
-
         {:ok, _} ->
+          nil
+
+        {:error, %Telegex.Error{description: "Bad Request: chat not found", error_code: 400}} ->
+          spawn(fn -> Subscribers.update_subscriber_status_to_unsubscribed(subscriber.chat_id) end)
+
+        {:error, _} ->
           nil
       end
     end)
   end
 
-  defp get_time_in_tashkent_now() do
+  defp uz_time_now() do
     DateTime.utc_now() |> DateTime.add(5, :hour)
   end
 end
